b3c5fde6d527012e8d63b8fd1d2909bff9585d07
ngoc dam <ngocdamvotran@gmail.com>
2016-12-20 00:07:27 -0800

    Add pj QLDoanhoi

---
+++ QLDoanhoi/QLDoanhoi/ChuongTrinh.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QLDoanhoi
+{
+    public class ChuongTrinh
+    {
+        public string Ma { get; set; }
+        public string Ten { get; set; }
+        public string Capdo { get; set; }
+        public string Thoigian { get; set; }
+ 
+        public ChuongTrinh(string MaChuongTrinh, string TenCT, string CapDoCT, string ThoiGianBD)
+        {
+            this.Ma = MaChuongTrinh;
+            this.Ten = TenCT;
+            this.Capdo = CapDoCT;
+            this.Thoigian = ThoiGianBD;
+        }   
+    }
+}
